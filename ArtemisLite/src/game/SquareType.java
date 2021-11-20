28467f324754f3075c0f778c9873f3b52745f257
Pierce Joseph Doherty <82386250+pdoherty1002@users.noreply.github.com>
2021-11-20 12:57:28 +0000

    Delete ArtemisLite directory

--- ArtemisLite/src/game/SquareType.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public enum SquareType {
-	
-	SPACE_LAUNCH_HQ,DEEP_SPACE,CHANCE,SYSTEM;
-
-}
--- ArtemisLite/src/game/SystemTypes.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public enum SystemTypes {
-	
-	RESEARCH,GROUND_SYSTEMS,LUNAR_EXPLORATION,SATELITES_SUPPORT;
-
-}
